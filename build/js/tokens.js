cec14384a2f5e1b95396fdc9814f3d5e30c8fcd5
Sanarielsen <Sanarielsen@users.noreply.github.com>
2023-05-02 05:21:36 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,9 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Tue, 02 May 2023 05:13:23 GMT
+ * Generated on Tue, 02 May 2023 05:21:35 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
